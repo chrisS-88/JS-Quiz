88bcd52ead5840395ce391bd1d82aa5182dd08ac
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-13 20:09:47 +0000

    initial commit, questions set and initialise ids

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+// Initialise ids
+const startScreenEl = document.getElementById("start-screen");
+const startButtonEl = document.getElementById("start");
+const questionsContainerEl = document.getElementById("questions");
+const questionTitleEl = document.getElementById("question-title");
+const choicesEl = document.getElementById("choices");
+const endScreenEl = document.getElementById("end-screen");
+const feedbackEl = document.getElementById("feedback");
---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    question: "What is the purpose of the 'let' keyword in JavaScript?",
+    options: [
+      "Declares a variable with block scope",
+      "Declares a constant variable",
+      "Declares a global variable",
+      "Declares a variable with function scope",
+    ],
+    answer: "Declares a variable with block scope",
+  },
+  {
+    question: "How can you check if a variable is of type 'number' in JavaScript",
+    options: ["typeof variable === 'number'", "variable instanceof Number", "variable.type === 'number'", "variable.isNumber()"],
+    answer: "typeof variable === 'number'",
+  },
+  {
+    question: "What does the JSON.parse() function do in JavaScript?",
+    options: ["Converts a string to a JSON object", "Converts a JSON object to a string", "Parses HTML content", "Encodes URL parameters"],
+    answer: " Converts a string to a JSON object",
+  },
+];
