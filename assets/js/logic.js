59fdee2c14e43eb69eec690d6f060bb82399009c
Chris Smart <Chris@Chriss-MacBook-Pro.local>
2024-01-14 00:06:00 +0000

    timer set, not working preperly

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,18 @@
 // Initialise ids
 const startScreenEl = document.getElementById("start-screen");
 const startButtonEl = document.getElementById("start");
+const timerEl = document.getElementById("time");
 const questionsContainerEl = document.getElementById("questions");
 const questionTitleEl = document.getElementById("question-title");
 const choicesEl = document.getElementById("choices");
 const endScreenEl = document.getElementById("end-screen");
 const feedbackEl = document.getElementById("feedback");
+const finalScore = document.getElementById("final-score");
 
 let currentQuestionIndex = 0;
 
 function setQuestion() {
+  timer();
   startScreenEl.classList.add("hide");
   questionsContainerEl.classList.remove("hide");
   feedbackEl.classList.remove("hide");
@@ -31,16 +34,59 @@ function selectAnswer(selectedOption) {
     feedbackEl.textContent = "Correct!";
   } else {
     feedbackEl.textContent = "Wrong!";
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      setQuestion();
+    } else {
+      questionsContainerEl.classList.add("hide");
+      feedbackEl.classList.add("hide");
+      endScreenEl.classList.remove("hide");
+    }
   }
+}
 
-  currentQuestionIndex++;
+// timer
+var secondesLeft = 5;
 
-  if (currentQuestionIndex < questions.length) {
-    setQuestion();
-  } else {
-    questionsContainerEl.classList.add("hide");
+function timer() {
+  let timerInterval = setInterval(function () {
+    secondesLeft--;
+    timerEl.textContent = secondesLeft;
+    if (secondesLeft === 0) {
+      clearInterval(timerInterval);
+      questionsContainerEl.classList.add("hide");
+      feedbackEl.classList.add("hide");
+      endScreenEl.classList.remove("hide");
+    }
+  }, 1000);
+}
+
+function score() {
+  finalScore.textContent = secondesLeft;
+}
+
+// time out message and choice to try again or quit
+// =======================================
+// function timerRunOutMessage() {
+//   reset();
+
+//   let tryAgainButtons = ["Yes", "No"];
+
+//   questionTitleEl.textContent = "You ran out of time, Would you like to try again?";
+
+//   tryAgainButtons.forEach((option, index) => {
+//     let button = document.createElement("button");
+//     button.textContent = option;
+//     choicesEl.appendChild(button);
+//     button.addEventListener("click", ()=>{});
+//   });
+// }
+
+// clear question if the time runs out
+function reset() {
+  while (choicesEl.firstChild) {
     feedbackEl.classList.add("hide");
-    endScreenEl.classList.remove("hide");
+    choicesEl.removeChild(choicesEl.firstChild);
   }
 }
 
